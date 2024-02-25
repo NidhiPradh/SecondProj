5bfeedc832bbb871061cfa680fbc278b06b66fe4
nidhi <nidhipradhan54@gmail.com>
2024-02-25 15:32:40 +0530

    Added 3 components Dashboard,Header,Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', component: ProductComponent },
+  { path: 'home', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
